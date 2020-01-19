4c10f19868648d08f13fe87e9fec4df9e5295924
Petros Demetriou <dimitriupe@gmail.com>
2020-01-19 22:08:24 +0200

    1st commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+const allImages = document.querySelectorAll('img');
+console.log(allImages);
+
+// add event mouseover
+allImages.forEach(images => {
+	images.addEventListener('mouseover', (event) => {
+        event.target.style.transform = 'scale(1.2)';
+        event.target.style.transition = 'all 0.3s';
+	});
+});
+
+// add event mouseout
+allImages.forEach(images => {
+	images.addEventListener('mouseout', (event) => {
+        event.target.style.transform = 'scale(1)';
+	});
+});
